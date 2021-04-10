44094565476b921326725c07620b6890ca1b9a6b
Shayer Mahmud Sowmik <45136656+BloodySowmik@users.noreply.github.com>
2021-04-10 18:48:40 +0600

    Create 2.sh

---
+++ 2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo $1 $2 $3
+len=("$3")
+num=("$2")
+blpin(){
+#i=1
+for i in $( eval echo {0..${len}} )
+do
+curl "https://circle.happycell.mobi/mylife/appapi/appcall.php?op=getOTC&pin=19823&app_version=75&msisdn='${num}'" --header 'x-app-key: 0skgg0ksg4880840w8w4gco0k4wckw0cg0w0w88s'
+#i=$(($i + 1))
+done
+}
+
+
+if [ ${1} == "bl" ]
+then
+   blpin
+elif [ ${1} == "rb" ]
+then
+    rbpin
+else
+     echo "Invalid Arguement ${1}"
+     exit 1
+fi
